7ea4813552b1a2fd4490e32f1b79bf05867f4715
Piotr C. <piotr@ciarka.net>
2024-11-11 12:41:48 +0100

    wersja z langfuse

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from getpass import getpass
 from IPython.display import Image
 import instructor
 from pydantic import BaseModel
-from openai import OpenAI
+# from openai import OpenAI
 import pandas as pd
 from dotenv import load_dotenv
 import os
 from pycaret.regression import  load_model, predict_model
 from typing import Optional
+import streamlit as st
+
+from langfuse.decorators import observe
+from langfuse.openai import OpenAI
 
 # załadowanie .env i wczytanie klucza
 load_dotenv()
@@ -23,9 +27,10 @@ class Desc_of_men(BaseModel):
     
 # dane wejściowe 
 # input()="jestem mężczyzną, mam 100 lat przebiegam 5km w 30 minut" 
-user_input = input("Napisz o sobie: ")
+#user_input = input("Napisz o sobie: ")
 
 # funkcja  - pytanie do AI
+@observe()
 def ask_AI (input):
     instructor_openai_client = instructor.from_openai(OpenAI(api_key=key))
 
@@ -38,9 +43,11 @@ def ask_AI (input):
                 "content": [
                     {
                         "type": "text",
-                        "text": """Wypełnij klasę Desc_of_men  
-                        wiek zaokrąglony do pełnych dziesięcioleci w dół i czas przebiegnięcia 5 km przeliczony na sekundy,
-                        nie zmyślaj danych jak nie wiesz zwróć wtedy None
+                        "text": """Wypełnij klasę Desc_of_men:  
+                        -Age_category wiek zaokrąglony do pełnych dziesięcioleci w dół
+                        Gender płeć 0 lub 1
+                        Time_5_km czas przebiegnięcia 5 km przeliczony na sekundy
+                        nie zmyślaj danych, jak nie wiesz zwróć wtedy None
                         Twój input: """+ input,
                     },
                     # {
@@ -56,30 +63,30 @@ def ask_AI (input):
     )
     return resp_AI
 
+#Sprawdzenie, czy któraś z kolumn ma brakującą wartość (NaN)
 def check_AI_response(df):
-    # Sprawdzenie, czy któraś z kolumn ma brakującą wartość (NaN)
-    # if pd.isna(df.loc[0, 'Age_category']) or pd.isna(df.loc[0, 'Gender']) or pd.isna(df.loc[0, 'Time_5_km']):
-    #     missing_info = df.loc[0, 'Info']
-    #     print(f"Brakuje mi danych, opowiedz więcej o: {missing_info}")
-    #     return False
+    if pd.isna(df.loc[0, 'Age_category']) or pd.isna(df.loc[0, 'Gender']) or pd.isna(df.loc[0, 'Time_5_km']):
+        return False
+    return True
+
+# Wydruk czego brakuje
+def check_AI_response_info(df):
     missing_info = ""
     
     if pd.isna(df.loc[0, 'Age_category']):
-        missing_info += "opowiedz o swoim wieku, "
+        missing_info += f"<br> coś o Twoim wieku, "
     if pd.isna(df.loc[0, 'Gender']):
-        missing_info += "jaką masz płeć, "
+        missing_info += f"<br>  o Twojej płci, "
     if pd.isna(df.loc[0, 'Time_5_km']):
-        missing_info += "jak idzie Ci bieganie na 5 km, "
+        missing_info += f"<br> jak idzie Ci bieganie na 5 km, "
     
     # Jeżeli missing_info nie jest pusty, znaczy że są brakujące dane
     if missing_info:
         # Usunięcie ostatniego przecinka i spacji
         missing_info = missing_info.rstrip(', ')
-        print(f"Brakuje mi danych: {missing_info}?")
-        return False
+        return st.markdown(f"Potrzebuję informacji, dopisz je: {missing_info}?", unsafe_allow_html=True)
     else:
-        print("Wszystkie dane są obecne.")
-        return True
+        return st.write("Dziękuję - mam wszystkie potrzebne informacje.")
     
 # funkcja sprawdzenie odpowiedzi z AI i dopytka lub podanie wyniku przetworzenia modelu
 def predict(df):
@@ -100,23 +107,42 @@ def convert_seconds(total_seconds):
     seconds = int(total_seconds % 60)           # Pełne sekundy
     return hours, minutes, seconds
 
-# zapytanie do AI
-df = ask_AI(user_input)
-data_list = [df]
-print(data_list)
-df = pd.DataFrame([data.dict() for data in data_list])
+st.title("Cześć, sprawdzę dla Ciebie jaki czas osiągniesz w półmaratonie.")
+st.subheader("""Opowiedz mi trochę o sobie: jesteś kobietą czy mężczyzną, ile masz lat oraz jaki masz aktualny czas na 5 km""")
 
-while not check_AI_response(df):
-    user_input = input("Napisz o sobie: ")
-    df = ask_AI(user_input)
-    data_list = [df]
-    print(data_list)
-    df = pd.DataFrame([data.dict() for data in data_list])
+# Dane początkowe pusty df załadowany do session state
+if 'df' not in st.session_state:
+    data = {
+    "Age_category": [None],  # przykładowe kategorie wiekowe
+    "Gender": [None],  # przykładowe płcie
+    "Time_5_km": [None]  # przykładowy czas na 5 km w minutach
+    }
+    st.session_state.df =  pd.DataFrame(data)
+df= st.session_state.df
 
-a=predict(df)
+if "user_input" not in st.session_state:
+    st.session_state.user_input = ''
+user_input = st.session_state.user_input
+#st.write(st.session_state)  
+       
+# Właściwy kod apki to poniższe dwa if'y            
 
-total_seconds = a.loc[0, 'Predicted_time_of_halfmarathon']
-hours, minutes, seconds = convert_seconds(total_seconds)
-print(f"{hours}h {minutes}min {seconds}sek Powodzenia w biegu!")
+user_input = st.text_input("Napisz o sobie:", st.session_state.user_input, key=f"input_2")
+if st.button("Zatwierdź", key=f"input_3"):
+        df = ask_AI(user_input)
+        df = pd.DataFrame([df.dict()])
+        while not check_AI_response(df):
+            #st.write(df) #tymczasowy wydruk tabeli z danymi
+            check_AI_response_info(df) # info dla usera o czym opowiedzieć
+            break
+      
+if check_AI_response(df): 
+    a=predict(df)
+    #sst.write(a) #tymczasowy wydruk odpowiedzi AI
+    total_seconds = a.loc[0, 'Predicted_time_of_halfmarathon']
+    hours, minutes, seconds = convert_seconds(total_seconds)
+    your_time=(f"Przewidywany czas ukończenia półmaratonu to: {hours}h {minutes}min {seconds}sek \n Powodzenia w biegu!")
+    st.write(your_time)
+
 
 
